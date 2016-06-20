8da9929f5b070e2f717b9c09a91149853ff6f7ef
Judy Long <long.judy92@gmail.com>
2016-06-20 08:48:07 -0400

    setup backbone with backbone-on-rails gem

---
+++ app/assets/javascripts/sky_cast.js
@@ -0,0 +1,13 @@
+window.SkyCast = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  SkyCast.initialize();
+});
