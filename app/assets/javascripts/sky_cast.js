542f976185f3262e3855becd302ae9932f7ea93b
Judy Long <long.judy92@gmail.com>
2016-06-21 09:07:36 -0400

    create a SkyCast namespaced weather model

--- app/assets/javascripts/sky_cast.js
+++ app/assets/javascripts/sky_cast.js
@@ -4,8 +4,11 @@ window.SkyCast = {
   Views: {},
   Routers: {},
   initialize: function() {
+    SkyCast.Models.weather = new SkyCast.Models.Weather();
+
     this.header = new SkyCast.Views.Header({el: $("#header")});
     this.router = new SkyCast.Routers.Router({$rootEl: $("#main")});
+
     Backbone.history.start();
   }
 };
