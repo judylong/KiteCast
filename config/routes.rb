411d2d6cf57e375164fc759fc3483784c28a1406
Judy Long <long.judy92@gmail.com>
2016-06-21 14:09:41 -0400

    update routes for users and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "site#root"
 
   namespace :api, defaults: {format: :json} do
+    resources :users, only: [:create, :show]
+    resource :session, only: [:create, :show, :destroy]
     resources :weather, only: [:index]
   end
 
