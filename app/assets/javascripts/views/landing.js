b5f530d9ed73ce67ac76122adaf63956bee8905b
Judy Long <long.judy92@gmail.com>
2016-06-21 14:19:38 -0400

    create landing view and template

---
+++ app/assets/javascripts/views/landing.js
@@ -0,0 +1,8 @@
+SkyCast.Views.Landing = Backbone.View.extend({
+  template: JST['landing'],
+  render: function() {
+    var content = this.template();
+    this.$el.html(content);
+    return this;
+  }
+});
