d77dddc2cfba2fec315068d59a677eda8edba075
Judy Long <long.judy92@gmail.com>
2016-06-21 14:29:32 -0400

    update router for logging in/signing up, and landing page

--- app/assets/javascripts/routers/router.js
+++ app/assets/javascripts/routers/router.js
@@ -4,7 +4,11 @@ SkyCast.Routers.Router = Backbone.Router.extend({
   },
 
   routes: {
-    "":"displayResult"
+    "":"landing",
+    "users/new":"newUser",
+    "history":"showHistory",
+    "display":"displayResult",
+    "session/new":"logIn",
   },
 
   displayResult: function() {
@@ -12,9 +16,54 @@ SkyCast.Routers.Router = Backbone.Router.extend({
     this._swapView(view);
   },
 
+  landing: function() {
+    var view = new SkyCast.Views.Landing();
+    this._swapView(view);
+  },
+
+  newUser: function() {
+    if (!this._requireLoggedOut()) { return; }
+    var model = new SkyCast.Models.User();
+    var view = new SkyCast.Views.UsersForm({
+      model: model
+    });
+    this._swapView(view);
+  },
+
   _swapView: function(view) {
     this._currentView && this._currentView.remove();
     this._currentView = view;
     this.$rootEl.html(this._currentView.render().$el);
+  },
+
+  logIn: function(callback) {
+    if (!this._requireLoggedOut(callback)) { return; }
+
+    var view = new SkyCast.Views.LogIn({
+      callback: callback
+    });
+    this._swapView(view);
+  },
+
+  _requireLoggedIn: function(callback) {
+    if (!SkyCast.currentUser.isLoggedIn()) {
+      callback = callback || this._goHome.bind(this);
+      this.logIn(callback);
+      return false;
+    }
+    return true;
+  },
+
+  _requireLoggedOut: function(callback){
+    if (SkyCast.currentUser.isLoggedIn()) {
+      callback = callback || this._goHome.bind(this);
+      callback();
+      return false;
+    }
+    return true;
+  },
+
+  _goHome: function() {
+    Backbone.history.navigate("#", { trigger: true })
   }
 });
