2c3e333ce4759d3ec7567129afcd885e79e97163
Judy Long <long.judy92@gmail.com>
2016-06-21 16:47:11 -0400

    store location queries when user queries a location for weather

--- app/controllers/api/weather_controller.rb
+++ app/controllers/api/weather_controller.rb
@@ -3,7 +3,15 @@ module Api
     def index
       if params[:query][:latitude] && params[:query][:longitude]
         @weather = Weather.new(params[:query])
+        if current_user
+          @query = LocationQuery.create(loc_query_params.merge(:user_id => current_user.id, :session_token => session[:session_token]))
+        end
+        @weather
       end
+    end
+
+    def loc_query_params
+      params[:query].permit(:latitude, :longitude, :formatted_address)
     end
   end
 end
