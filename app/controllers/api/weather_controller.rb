229e0271156453927d4787e460e030bae81f7cf6
Judy Long <long.judy92@gmail.com>
2016-06-22 00:31:02 -0400

    stray debugger

--- app/controllers/api/weather_controller.rb
+++ app/controllers/api/weather_controller.rb
@@ -2,7 +2,6 @@ module Api
   class WeatherController < ApiController
     def index
       if params[:query][:latitude] && params[:query][:longitude]
-        debugger
         @weather = Weather.new(params['query'])
         if current_user
           @query = LocationQuery.create(loc_query_params.merge(:user_id => current_user.id, :session_token => session[:session_token]))
